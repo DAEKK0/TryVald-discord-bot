d505cedcde92417fd2de6ef2f5cd7372f08464c1
DAEKK0 <gangcaponebeige@gmail.com>
2026-03-03 01:45:39 +0300

    paste the invite link of the bot

---
+++ src/commands/info/link.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('link')
+    .setDescription('Get the invite link to add this bot to another server.'),
+  async execute(interaction) {
+    const link = 'https://discord.com/oauth2/authorize?client_id=1476990664204882062&permissions=8&integration_type=0&scope=bot+applications.commands';
+
+    const embed = new EmbedBuilder()
+      .setColor('Blue')
+      .setTitle('🔗 Invite Me!')
+      .setDescription('Click the link below to add this bot to your own server:')
+      .addFields({ name: 'Invite Link', value: link })
+      .setFooter({ text: 'Requires Administrator permissions to function fully.' });
+
+    await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral }); // Ephemeral so only the user sees it
+  },
+};
